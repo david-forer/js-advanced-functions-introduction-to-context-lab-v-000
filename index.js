125bbb6c68ab02ac403567e3c6f7f02cdee94a4c
David Forer <david.j.forer@gmail.com>
2019-11-21 06:40:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function createEmployeeRecord(employeeData) {
+  let newEmployee = {};
+  
+  newEmployee.firstName = employeeData[0];
+  newEmployee.familyName = employeeData[1];
+  newEmployee.title = employeeData[2];
+  newEmployee.payPerHour = employeeData[3];
+  newEmployee.timeInEvents = [];
+  newEmployee.timeOutEvents = [];
+  
+  return newEmployee;
+}
+
+function createEmployeeRecords(employeesArray) {
+  return employeesArray.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+  let timeInObj = {
+    type: "Timein",
+    hour: parseInt(dateStamp.slice(11, 16),10),
+    date:dateStamp.slice(0,10)
+  }
+  employeeRecord.timeInEvents.push(timeInObj);
+  return employeeRecord
+}
